314422038c13879dafc29a329611b5ee9edf0100
magnolppacheco <magnolppacheco@gmail.com>
2022-08-03 10:26:35 -0300

    atualizações iniciais

--- index.js
+++ index.js
@@ -7,33 +7,35 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const linguagens = [
+const termos = [
   {
     id: 1,
     termo: 'Javascript',
-    descricao: 'lalalala',
-    logo: 'caminho imagem',
-    ano_de_criacao: '2022',
+    descricao:
+      'JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma.',
+    logo: './assets/images/js.png',
+    ano_de_criacao: '1995',
   },
   {
     id: 2,
     termo: 'Python',
-    descricao: 'lalala',
-    logo: 'caminho imagem',
-    ano_de_criacao: '2022',
+    descricao:
+      'Python é uma linguagem de programação de alto nível, interpretada de script, imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte.',
+    logo: './assets/images/python.png',
+    ano_de_criacao: '1991',
   },
-
   {
     id: 3,
     termo: 'Java',
-    descricao: 'lalala',
-    logo: 'caminho imagem',
-    ano_de_criacao: '2022',
+    descricao:
+      'Java é uma linguagem de programação orientada a objetos desenvolvida na década de 90 por uma equipe de programadores chefiada por James Gosling, na empresa Sun Microsystems, que em 2008 foi adquirido pela empresa Oracle Corporation.',
+    logo: './assets/images/java.png',
+    ano_de_criacao: '1991',
   },
 ];
 
-app.get('/linguagens/todas-linguagens', (req, res) => {
-  res.send(linguagens);
+app.get('/termos/todos-termos', (req, res) => {
+  res.send(termos);
 });
 
 app.listen(port, () => {
